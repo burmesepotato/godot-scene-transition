0cf223e76b2f7b226716d1120e438729f6844742
Si Thu <sithu.mdy.07@gmail.com>
2025-08-18 00:30:16 +0700

    Adds ColorRect as SceneTransition

---
+++ scripts/living_room.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+#@onready var _transition_rect := $SceneTransitionRect
+@onready var _transition_rect : ColorRect = $Scene
+
+func _on_spaceWindow_clicked(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event.is_action_pressed("MouseLeftClick"):
+		print("clicked")
+		_transition_rect.transition_to("res://scenes/space.tscn")
+
+
+func _on_spaceWindow_mouse_entered() -> void:
+	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+
+
+func _on_spaceWindow_mouse_exited() -> void:
+	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
